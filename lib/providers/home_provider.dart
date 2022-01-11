aba53b13073a688e2218a753eb817252a541134f
TaurusLegend <claudia_filipa45@outlook.com>
2022-01-11 03:14:05 +0000

    Filter users by nickname without needing to write the full nickname

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -23,11 +23,17 @@ class HomeProvider {
 
   Stream<QuerySnapshot> getStreamFireStore(
       String pathCollection, int limit, String? textSearch) {
-    if (textSearch?.isNotEmpty == true) {
+    if (textSearch != null && textSearch.isNotEmpty == true) {
       return firebaseFirestore
           .collection(pathCollection)
           .limit(limit)
-          .where(FirestoreConstants.nickname, isEqualTo: textSearch)
+          .where(
+            FirestoreConstants.nickname,
+            isGreaterThanOrEqualTo: textSearch,
+            isLessThan: textSearch.substring(0, textSearch.length - 1) +
+                String.fromCharCode(
+                    textSearch.codeUnitAt(textSearch.length - 1) + 1),
+          )
           .snapshots();
     } else {
       return firebaseFirestore
