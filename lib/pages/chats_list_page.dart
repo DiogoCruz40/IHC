e9107443b1bfbb1f9e9a3d5b1e338efb1afefe6b
Diogo Cruz <diogo.e.cruz@hotmail.com>
2022-01-14 22:30:55 +0000

    chat and android 12 half fixed

--- lib/pages/chats_list_page.dart
+++ lib/pages/chats_list_page.dart
@@ -47,6 +47,7 @@ class _ChatsListPageState extends State<ChatsListPage> {
     currentuserId = widget.currentuserId;
     peersmessages = List.empty(growable: true);
     idslist = List.empty(growable: true);
+
     // peersmessages = [''];
     //idslist = [''];
     super.initState();
